a746a97c35e913027c01254ccb624ef6c6965baa
Hugo Losada <129153062+Yodugo@users.noreply.github.com>
2025-05-24 19:43:23 +0200

    Add files via upload

---
+++ src/Pages/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+
+namespace PracticalWork_2
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+
+            Routing.RegisterRoute("LoginPage", typeof(Pages.LoginPage));
+            Routing.RegisterRoute("RegisterPage", typeof(Pages.RegisterPage));
+            Routing.RegisterRoute("RecoverPasswordPage", typeof(Pages.RecoverPasswordPage));
+            Routing.RegisterRoute("MainPage", typeof(Pages.MainPage));
+            Routing.RegisterRoute("UserInfoPage", typeof(Pages.UserInfoPage));
+        }
+    }
+}
